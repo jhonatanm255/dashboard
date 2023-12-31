c13a3b58c7d0cd534f25a77dc9bbf2ef34e84058
Jhonatan muñoz <jhonm21@gmail.com>
2023-12-31 03:44:44 -0300

    Optimizacion para resolucion de 425 px

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ const body = document.body;
 
 toggleButton.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
-    body.classList.toggle('light-mode')
-
+    body.classList.toggle('light-mode');
 });
+
+    const btnHamburguesa = document.getElementById('btn-h');
+    const aside = document.querySelector('.light-mode'); 
+
+    btnHamburguesa.addEventListener('click', () => {
+        aside.classList.toggle('active-menu');
+    });
+
+
+
+
+
 
 
    
