fd2b92cc95a044f5642928fc809acc4c951e5809
jhonatanm255 <82849525+jhonatanm255@users.noreply.github.com>
2024-01-17 03:06:40 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -17,17 +17,40 @@ toggleButton.addEventListener('click', () => {
 
 
 // ENDPOINT PARA EL PRECIO DE LAS CRIPTOS
+function createSocket(symbol) {
+    const tradeSocket = new WebSocket(`wss://stream.binance.com:9443/ws/${symbol.toLowerCase()}@trade`);
+    const tickerSocket = new WebSocket(`wss://stream.binance.com:9443/ws/${symbol.toLowerCase()}@ticker`);
 
- let ws = new WebSocket('wss://stream.binance.com:9443/ws/etheur@trade');
- let stockPriceElement = document.getElementById('stock-price')
- let lastPrice = null;
+    const stockPriceElement = document.getElementById(`${symbol.toLowerCase()}-price`);
+    const volumenSpan = document.getElementById(`${symbol.toLowerCase()}-volumen`);
 
+    let lastPrice = null;
 
-ws.onmessage = (event) => {
-    let stockObject = JSON.parse(event.data);
-    let price = parseFloat(stockObject.p).toFixed(2);
-    stockPriceElement.innerText = price
-    stockPriceElement.style.color = !lastPrice || lastPrice === price ? 'black' : price > lastPrice ? 'green' : 'red';
-    lastPrice = price;
-};
+    // Manejar eventos para la conexión WebSocket de trades
+    tradeSocket.onmessage = (event) => {
+        const stockObject = JSON.parse(event.data);
+        const price = parseFloat(stockObject.p).toFixed(2);
+        stockPriceElement.innerText = `Price: ${price} $`;
+        stockPriceElement.style.color = !lastPrice || lastPrice === price ? 'black' : price > lastPrice ? 'green' : 'red';
+        lastPrice = price;
+    };
 
+    // Manejar eventos para la conexión WebSocket de ticker
+    tickerSocket.onmessage = (event) => {
+        const tickerData = JSON.parse(event.data);
+        volumenSpan.innerText = `Vol 24h: ${tickerData.P} %`;
+    };
+
+    return { tradeSocket, tickerSocket };
+}
+
+// Lista de criptomonedas que deseas seguir
+const cryptoSymbols = ['ethusdt', 'btcusdt', 'adausdt', 'dotusdt', 'bnbusdt', 'solusdt','ltcusdt', 'avaxusdt'];
+
+// Objeto para almacenar todas las conexiones WebSocket
+const cryptoSockets = {};
+
+// Crear conexiones para cada criptomoneda
+cryptoSymbols.forEach(symbol => {
+    cryptoSockets[symbol] = createSocket(symbol);
+});
